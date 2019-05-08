6317b1dac44111d5f7238d611d82039b7e8b0158
doubleMteam <double.m.team199@gmail.com>
2019-05-09 00:41:54 +0400

    New App ii

--- app/src/main/java/com/example/dell/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/dell/myapplication/RegistrationActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        Toast.makeText(this, "hi n", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hi n", Toast.LENGTH_SHORT).show();
     }
 }
